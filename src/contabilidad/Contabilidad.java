87114adfdb03fc02ca03c93d197c3c2ca65c9fc5
Vidal92 <vidal16.01.92@gmail.com>
2019-04-07 15:03:02 -0500

    version 1

---
+++ src/contabilidad/Contabilidad.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contabilidad;
+
+/**
+ *
+ * @author user
+ */
+public class Contabilidad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Inicio n = new Inicio();
+        n.setVisible(true);
+        n.setSize(458, 310);
+        n.setLocationRelativeTo(null);
+    }
+}
